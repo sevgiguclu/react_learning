4d5807f9fe262070917cf5f2116c650483cfb718
sevgiguclu <guclusevgi97@gmail.com>
2023-10-04 14:50:14 +0300

    props

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -5,9 +5,13 @@ import Home from "./components/Home";
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Home name="firstText"></Home>
       {/* props mantığı, istediğin isim ile gönderebilirsin aldığın yere de aynı ismi yazman şartıyla */}
+      {/* birden fzala props gönderebiliriz */}
+      <Header number="1" type="shoes"></Header>
+      {/* number 1 kategorisinde kıyafetler olsun */}
+      <Header number="2" type="business"></Header>
+      {/* number 2 kategroisinde şirket reklamlar */}
+      <Home name="firstText"></Home>
       <Home name="descriptionText"></Home>
 
 
--- first-react-project/src/components/Header.js
+++ first-react-project/src/components/Header.js
@@ -1,9 +1,33 @@
 
 
-function Header() {
+function Header({number,type}) {
+    // birden fazla props alabiliriz
+    var style;
+    var style1 = {
+        textAlign:'center',
+        backgroundColor:'#f96256',
+        color:'black',
+        margin:'10px',
+        padding:'10px'
+    }
+    var style2 = {
+        textAlign:'center',
+        backgroundColor:'#541256',
+        color:'white',
+        margin:'10px',
+        padding:'10px'
+    }
+    console.log(number,'number');
+    if(number == '1'){
+        style = style1;
+    }
+    if(number == '2'){
+        style = style2;
+    }
     return (
-        <div style={{textAlign:'center',backgroundColor:'#541256',color:'white',margin:'10px',padding:'10px'}}>
-            Header  Bölümü
+        <div style={style}>
+            <div>Header  Bölümü {number}</div>
+            <div>Reklam {type}</div>
         </div>
     );
   }
