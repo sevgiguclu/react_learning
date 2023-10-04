bfc8698def49e6b61f5022f26408ae17bc167f7e
sevgiguclu <guclusevgi97@gmail.com>
2023-10-04 14:37:47 +0300

    component and props

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,8 +1,17 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 
 function App() {
   return (
-    <div>Merhaba</div>
+    <div>
+      <Header></Header>
+      <Home name="firstText"></Home>
+      {/* props mantığı, istediğin isim ile gönderebilirsin aldığın yere de aynı ismi yazman şartıyla */}
+      <Home name="descriptionText"></Home>
+
+
+    </div>
   );
 }
 
---
+++ first-react-project/src/components/Header.js
@@ -0,0 +1,12 @@
+
+
+function Header() {
+    return (
+        <div style={{textAlign:'center',backgroundColor:'#541256',color:'white',margin:'10px',padding:'10px'}}>
+            Header  Bölümü
+        </div>
+    );
+  }
+  
+  export default Header;
+  
